9a063dc926df351321186001ddc23c5b6b8f4969
nyot1982 <marcpinyot@hotmail.com>
2025-06-07 13:54:22 +0200

    email

--- email.php
+++ email.php
@@ -7,65 +7,40 @@
         <meta http-equiv="Expires" content="0" />
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Last Mission</title>
+        <title>Last Mission email</title>
         <meta name="description" content="Last Mission">
         <meta name="keywords" content="last mission">
         <link rel="shortcut icon" href="favicon.ico">
         <link rel="apple-touch-icon" href="favicon.png">
         <link rel="stylesheet" href="css/game_styles.css" type="text/css">
         <script type="text/javascript" src="js/jquery-3.7.1.min.js"></script>
-        <script type="text/javascript" src="js/game_scripts.js"></script>
-        <?php
-        
-        ?>
     </head>
     <body>
-        <preloader><div class="spinner"></div></preloader>
         <main>
             <article>
                 <?php
-// Varios destinatarios
-$para  = 'marcpinyot@gmail.com'.', '; // atención a la coma
-$para .= 'marcpinyot@hotmail.com';
+                    // Varios destinatarios
+                    $para  = 'marcpinyot@gmail.com'.', '; // atención a la coma
+                    $para .= 'marcpinyot@hotmail.com';
 
-// título
-$título = 'Creación de cuenta online multiplayer';
+                    // título
+                    $título = 'Creación de cuenta online multiplayer';
 
-// mensaje
-$mensaje = '
-<html>
-<head>
-  <title>Creación de cuenta</title>
-</head>
-<body>
-  <p>¡Datos del registro!</p>
-  <table>
-    <tr>
-      <th>Quien</th><th>Día</th><th>Mes</th><th>Año</th>
-    </tr>
-    <tr>
-      <td>Marc</td><td>3</td><td>Agosto</td><td>1970</td>
-    </tr>
-    <tr>
-      <td>Marc</td><td>17</td><td>Agosto</td><td>1973</td>
-    </tr>
-  </table>
-</body>
-</html>
-';
+                    // mensaje
+                    $mensaje = '<html><head><title>Creación de cuenta</title></head><body><p>¡Datos del registro!</p><table><tr><th>Quien</th><th>Día</th><th>Mes</th><th>Año</th></tr><tr><td>Marc</td><td>3</td><td>Agosto</td><td>1970</td></tr><tr><td>Marc</td><td>17</td><td>Agosto</td><td>1973</td></tr></table></body></html>';
 
-// Para enviar un correo HTML, debe establecerse la cabecera Content-type
-$cabeceras  = 'MIME-Version: 1.0'."\r\n";
-$cabeceras .= 'Content-type: text/html; charset=utf-8'."\r\n";
+                    // Para enviar un correo HTML, debe establecerse la cabecera Content-type
+                    $cabeceras  = 'MIME-Version: 1.0'."\r\n";
+                    $cabeceras .= 'Content-type: text/html; charset=utf-8'."\r\n";
 
-// Cabeceras adicionales
-$cabeceras .= 'To: Marc Pinyot Gascón <marcpinyot@gmail.com>, Marc Pinyot Gascón <marcpinyot@hotmail.com>'."\r\n";
-$cabeceras .= 'From: Marc Pinyot Gascón <marcpinyot@gmail.com>'."\r\n";
+                    // Cabeceras adicionales
+                    $cabeceras .= 'To: Marc Pinyot Gascón <marcpinyot@gmail.com>, Marc Pinyot Gascón <marcpinyot@hotmail.com>'."\r\n";
+                    $cabeceras .= 'From: Marc Pinyot Gascón <marcpinyot@gmail.com>'."\r\n";
 
-// Enviarlo
-mail ($para, $título, $mensaje, $cabeceras);
-?>
-                <div id="blackScreen"></div>
+                    // Enviarlo
+                    mail ($para, $título, $mensaje, $cabeceras);
+                    //mb_send_mail ($para, $titulo, $mensaje, $cabeceras);
+                ?>
             </article>
         </main>
     </body>
