4f687454f987bbb0a61d8f274d1e64ee49fcd65b
nyot1982 <marcpinyot@hotmail.com>
2025-07-22 16:28:07 +0200

    login

--- fetch/signin.php
+++ fetch/signin.php
@@ -11,12 +11,17 @@
     if ($mysqli->connect_errno) $return ["error"] = 'Error! Conexion has failed: ('.$mysqli->connect_errno.') '.$mysqli->connect_error;
     else
     {
-        $resultado = $mysqli->query ('SELECT * FROM players WHERE email = "'.$_POST['email'].'" AND password = "'.$_POST['password'].'"');
+        $resultado = $mysqli->query ('SELECT * FROM players WHERE email = "'.$_POST ['email'].'" AND password = "'.$_POST ['password'].'"');
         if ($mysqli->errno) $return ["error"] = 'Error! Query has failed: ('.$mysqli->errno.') '.$mysqli->error;
         else
         {
-            $high_score = $resultado->fetch_row ();
-            $return ["highScoreHud"] = intval ($high_score [0]);
+            if ($resultado->num_rows == 0) $return ["error"] = 'Wrong credentials';
+            else
+            {
+                $player = $resultado->fetch_assoc ();
+                if ($player ['validated'] == 0) $return ["error"] = 'Account not validated';
+                else $return ["player"] = [ 'id' => intval ($player ['id']), 'validated' => intval ($player ['validated']), 'email' => $player ['email'], 'user' => $player ['user'], 'password' => $player ['password'], 'skins' => $player ['skins'] ];
+            }
             $resultado->free ();
         }
     }
